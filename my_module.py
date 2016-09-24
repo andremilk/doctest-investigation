0f715b485e93d7890afc8228d8ec1c19356748c4
Andre Leite <andreleite@Andres-Air.home>
2016-09-24 17:00:12 -0300

    First commit.

---
+++ my_module.py
@@ -0,0 +1,21 @@
+#!/usr/env python
+
+MY_DICT = {'key1': 'value1', 'key2': 'value2'}
+MY_STRING = 'My keys are: {}'.format(MY_DICT.keys())
+
+def my_method1():
+    """
+    >>> my_method1()
+    Traceback (most recent call last):
+        ...
+    Exception: My keys are: ['key2', 'key1']
+    """
+    raise Exception(MY_STRING)
+
+
+def my_method2():
+    pass
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
