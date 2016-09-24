e7c34c3aed81e556dd97c4c2b0dd7e96a85f8e01
Andre Leite <andreleite@Andres-Air.home>
2016-09-24 19:53:51 -0300

    Changing doctest

--- my_module.py
+++ my_module.py
@@ -7,15 +7,20 @@ def my_method1():
     """
     >>> my_method1()
     Traceback (most recent call last):
-        ...
+            ...
     Exception: My keys are: ['key2', 'key1']
     """
     raise Exception(MY_STRING)
 
 
 def my_method2():
+    """
+    >>> my_method1()
+    Exception: {MY_STRING}
+    """
     pass
 
 if __name__ == '__main__':
     import doctest
+    print doctest.__file__
     doctest.testmod()
